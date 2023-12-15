72fb93df018ed2983da50a7ebc84669b00dbf45c
hisame103018 <hisame103018@gmail.com>
2023-12-15 13:49:39 +0900

    my_add 함수 추가

--- Cal.java
+++ Cal.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-	    //주석추가
+	   //New Work
+	    int my_add(int number1, int number2){
+               if ( (number1 < 0 or number2 < 0)  && (number1 + number2 < 0) )
+                   return;
+               return number1+number2;
+	   }
     }
 }
